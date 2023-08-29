f1844615a0f31ce7998d9653460cb6c4d04ff83b
Will Hopkins <willothyh@gmail.com>
2023-08-29 14:04:47 -0700

    feat: fetch new recs when list is exhausted
    perf: use list pos instead of slicing on skip

--- web-client/src/screens/Dashboard.js
+++ web-client/src/screens/Dashboard.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { getPlaylistSelected, getPlaylistInfo, hasPlaylistSelected, getPlaylistRecommendations } from '../services/spotify';
+import { getPlaylistInfo, getPlaylistRecommendations } from '../services/spotify';
 import { Box, Chip, Skeleton } from '@mui/material';
 import SongTile from '../components/SongTile';
 import PlaylistsDialog from '../components/PlaylistsDialog';
@@ -10,78 +10,88 @@ function Dashboard() {
   const [selectedPlaylist, setSelectedPlaylist] = useState(null);
   const [playlistInfo, setPlaylistInfo] = useState(null);
   const [recommendationInfoList, setRecommendationInfoList] = useState([]);
+  const [listPos, setListPos] = useState(0);
 
-  const skipSong = () => {
-    setRecommendationInfoList((val) => val.slice(1));
+  const skipSong = () => setListPos((val) => val + 1);
+
+  const clearSelected = () => setSelectedPlaylist(null);
+
+  const hasSelection = () => recommendationInfoList.length > 0 && listPos < recommendationInfoList.length;
+
+  const fetch = async () => {
+    try {
+      const playlistInfo = await getPlaylistInfo(selectedPlaylist);
+      setPlaylistInfo(playlistInfo);
+      const recommendationInfo = await getPlaylistRecommendations(selectedPlaylist);
+      setRecommendationInfoList(recommendationInfo);
+      setListPos(0);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
 
+  // TODO: pre-fetch so data is ready immediately when user skips past the
+  // end of the current list
   useEffect(() => {
-    if (!hasPlaylistSelected()) {
-      setShowPlaylists(true);
+    if (listPos == recommendationInfoList.length) {
+      fetch();
     }
-    setSelectedPlaylist(getPlaylistSelected());
-  }, []);
+  }, [listPos]);
 
   useEffect(() => {
     if (selectedPlaylist) {
       setRecommendationInfoList([]);
       setShowPlaylists(false);
 
-      const fetch = async () => {
-        try {
-          const playlistInfo = await getPlaylistInfo(selectedPlaylist);
-          setPlaylistInfo(playlistInfo);
-          const recommendationInfo = await getPlaylistRecommendations(selectedPlaylist);
-          setRecommendationInfoList(recommendationInfo);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      }
       fetch();
+    } else {
+      setRecommendationInfoList([]);
+      setPlaylistInfo(null);
+      setShowPlaylists(true);
     }
   }, [selectedPlaylist]);
 
   return (
     <>
-    {showPlaylists && <PlaylistsDialog setSelectedPlaylist={setSelectedPlaylist} />}
-    <Box sx={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-      flexDirection: 'column',
-      gap: '.5rem',
-    }}
-    >
-      <Box>
-        <Box  sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          alignItems: 'center',
-          justifyContent: 'left',
-          flexDirection: 'row',
-          gap: '.5rem',
-        }}
-        >
-          <Chip label={playlistInfo?.name} variant="outlined" onClick={() => {setSelectedPlaylist(null); setShowPlaylists((val) => !val)}} />
+      {showPlaylists && <PlaylistsDialog setSelectedPlaylist={setSelectedPlaylist} />}
+      <Box sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        flexDirection: 'column',
+        gap: '.5rem',
+      }}
+      >
+        <Box>
+          <Box sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            justifyContent: 'left',
+            flexDirection: 'row',
+            gap: '.5rem',
+          }}
+          >
+            <Chip label={playlistInfo?.name} variant="outlined" onClick={clearSelected} />
+          </Box>
+          {hasSelection() ? <SongTile song={recommendationInfoList[listPos]} playlistId={selectedPlaylist} skipSong={skipSong} /> :
+            <>
+              <Skeleton variant="rectangular" sx={{ m: 1 }} width={600} height={300} />
+              <Box sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'right',
+                flexDirection: 'row',
+                width: 600,
+              }} >
+                <Skeleton variant="text" sx={{ fontSize: '1.5rem', width: 150 }} />
+              </Box>
+            </>}
         </Box>
-        {recommendationInfoList.length > 0 ? <SongTile song={recommendationInfoList[0]} playlistId={selectedPlaylist} skipSong={skipSong} /> : 
-        <>
-          <Skeleton variant="rectangular" sx={{m: 1}} width={600} height={300} />
-            <Box sx={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              alignItems: 'center',
-              justifyContent: 'right',
-              flexDirection: 'row',
-              width: 600,
-            }} >
-              <Skeleton variant="text" sx={{ fontSize: '1.5rem', width: 150 }}/>
-            </Box>
-          </>}
       </Box>
-    </Box>
     </>
   );
 }
